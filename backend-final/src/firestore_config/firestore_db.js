b8f07faeed33ffe94d52731526bf13d4215d6fbb
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-10-25 21:53:54 -0300

    js

--- backend-final/src/firestore_config/firestore_db.js
+++ backend-final/src/firestore_config/firestore_db.js
@@ -2,7 +2,7 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
-
+//conexion de firestore
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
